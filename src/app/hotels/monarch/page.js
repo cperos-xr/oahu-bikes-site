ecaf6881c7c00293c9a9c3e48bb6ee4ec27fee2c
cperos-xr <cperos.xr@gmail.com>
2025-12-05 17:29:38 -1000

    Fix lint errors for Vercel build

--- src/app/hotels/monarch/page.js
+++ src/app/hotels/monarch/page.js
@@ -70,7 +70,7 @@ export default function MonarchPage() {
             <div className="h-6 border-r border-slate-300" />
             <div className="flex items-center gap-3">
               <a href={hotel.website} target="_blank" rel="noreferrer" className="hover:opacity-80 transition-opacity">
-                <img 
+                <Image 
                   src={hotel.logo}
                   alt={`${hotel.name} Logo`}
                   className="h-8 w-auto object-contain"
